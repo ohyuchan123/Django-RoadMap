48b858c5db920faf0d7a2e2cb49d8a5ffd749b47
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-01-22 01:58:38 +0900

    :sparkles: model 만들기

--- Django Tutorial/pybo/polls/models.py
+++ Django Tutorial/pybo/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField(("date published"), auto_now=False, auto_now_add=False)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
