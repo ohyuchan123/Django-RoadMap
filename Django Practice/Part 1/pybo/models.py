3010433c4141b0c40e8d5a3a39234d7455f679eb
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-04-05 01:33:05 +0900

    :sparkles: 장고 관리자

--- Django Practice/Part 1/pybo/models.py
+++ Django Practice/Part 1/pybo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    subject = models.CharField(max_length = 200)
+    subject = models.CharField(max_length = 200) # 제목처럼 글자수의 길이가 제한된 텍스트는 CharField를 사용한다.
     content = models.TextField()
     create_date = models.DateTimeField()
 
