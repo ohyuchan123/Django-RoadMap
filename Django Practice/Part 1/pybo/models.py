d4c1c283edef4dd4ed2de5360d3dfed4798ff6bb
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-04-07 20:59:52 +0900

    :sparkles: 모델 변경

--- Django Practice/Part 1/pybo/models.py
+++ Django Practice/Part 1/pybo/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length = 200) # 제목처럼 글자수의 길이가 제한된 텍스트는 CharField를 사용한다.
     content = models.TextField()
     create_date = models.DateTimeField()
@@ -10,6 +12,8 @@ class Question(models.Model):
         return self.subject
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+     #on_delete=models.CASCADE는 계정이 삭제되면 이 계정이 작성한 질문을 모두 삭제하라는 의미입니다.
     content = models.TextField()
     create_date = models.DateTimeField()
--- Django Practice/Part 1/pybo/views.py
+++ Django Practice/Part 1/pybo/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 
-from django.http import HttpResponseNotAllowed 
+# from django.http import HttpResponseNotAllowed 
 
 from .models import Question
 from .forms import QuestionForm,AnswerForm
 
 from django.core.paginator import Paginator # 장고 페이징을 위해서 사용하는 클래스는 Paginator입니다.
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -27,27 +28,34 @@ def detail(request,question_id):
     context = {'question':question}
     return render(request,'pybo/question_detail.html',context)
 
+@login_required(login_url='common:login')
 def answer_create(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
+        # 이제 로그아웃 상태에서 "답변등록" 버튼을 누르더라도 로그인 수행후 405 오류가 발생하지 않고 상세화면으로 잘 돌아가는 것을 확인할 수 있다.
+
+        # return HttpResponseNotAllowed('Only POST is possible.') -> 이전에는 이 설정 때문에 405 오류가 발생했었음.
         # 답변 등록은 POST 방식만 사용되기 때문에 GET 방식으로 요청할 경우에는 HttpResponseNotAllowed 오류가 발생하도록 했다.
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():  # 폼이 유효하다면
             question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴받는다.
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정한다.
             question.save()  # 데이터를 실제로 저장한다.
             return redirect('pybo:index')
